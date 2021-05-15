755a49d4c782c7541cd1c26f44fea4a06752aa39
Alan Casas <alancasasarevalo@gmail.com>
2021-05-15 15:54:07 +0200

    Initial Commit

---
+++ Side_Menu_in_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Side_Menu_in_SwiftUI
+//
+//  Created by Alan Casas on 15/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Side_Menu_in_SwiftUI/Side_Menu_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Side_Menu_in_SwiftUIApp.swift
+//  Side_Menu_in_SwiftUI
+//
+//  Created by Alan Casas on 15/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Side_Menu_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
