e7153dcae1d3cb1e85b3f73289eade7525572f98
Alan Casas <alancasasarevalo@gmail.com>
2021-05-15 16:17:27 +0200

    Side menu header was added

--- Side_Menu_in_SwiftUI/SideMenuHeaderView.swift
+++ Side_Menu_in_SwiftUI/SideMenuHeaderView.swift
@@ -2,7 +2,38 @@ import SwiftUI
 
 struct SideMenuHeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .leading) {
+            Image(systemName: "list.bullet")
+                .resizable()
+                .scaledToFill()
+                .clipped()
+                .frame(width: 64, height: 64)
+                .clipShape(Circle())
+                .padding(.bottom, 16)
+                
+            Text("Eddie Gordo")
+                .font(.system(size: 24, weight: .semibold, design: .rounded))
+
+            Text("@egordo")
+                .font(.system(size: 14, weight: .light, design: .rounded))
+                .padding(.bottom, 24)
+
+            HStack (spacing: 12) {
+                HStack (spacing: 4) {
+                    Text("24").bold()
+                    Text("Following")
+                }
+                HStack (spacing: 4) {
+                    Text("42").bold()
+                    Text("Followers")
+                }
+                
+                Spacer()
+            }
+            
+            Spacer()
+        }
+        .padding()
     }
 }
 
